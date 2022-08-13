536c8a96891f5645b8d0565ef11021fa6027bbdd
Andrew - PC <keekyam07@gmail.com>
2022-08-13 11:03:02 +0800

    Added code for hiding the navigation bar

--- app/src/main/java/com/example/todonato/Splash.java
+++ app/src/main/java/com/example/todonato/Splash.java
@@ -3,6 +3,7 @@ package com.example.todonato;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,12 +17,20 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
         fAuth = FirebaseAuth.getInstance();
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+
+
 
                 //Check if user if logged in
                 if(fAuth.getCurrentUser() != null){
